c48a95b2624b0478861bef4d24a973ec4384196c
Marin Petrunić <marin.petrunic@gmail.com>
2018-08-31 17:50:33 +0200

    removes extra styles and tests

--- spec/atom-solidity-comments-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import AtomSolidityCommentsView from '../lib/atom-solidity-comments-view';
-
-describe('AtomSolidityCommentsView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
